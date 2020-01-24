eb82d7c6e0f4deb632220a43b14d71c7ec5220f1
Tim <tim@Tims-MacBook-Air.local>
2020-01-23 17:54:16 -0700

    started to build firestore crud functionality and view db records

---
+++ lib/ListBuilder/firebase_crud.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final databaseReference = Firestore.instance;
+  
+List<String> items = [];
+
+void createRecord() async {
+  await databaseReference.collection("books")
+      .document("1")
+      .setData({
+        'title': 'Mastering Flutter',
+        'description': 'Programming Guide for Dart'
+      });
+
+  DocumentReference ref = await databaseReference.collection("books")
+      .add({
+        'title': 'Flutter in Action',
+        'description': 'Complete Programming Guide to learn Flutter'
+      });
+  print(ref.documentID);
+}
+
+void getData() {
+  //print(databaseReference.collection("books"));
+  databaseReference
+      .collection("books")
+      .getDocuments()
+      .then((QuerySnapshot snapshot) {
+    snapshot.documents.forEach((f) => items.add('${f.data}}'));
+  });
+  print(items);
+}
+
+class FirestoreData extends StatelessWidget {
+  
+  @override
+     Widget build(BuildContext context) {
+       return Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          RaisedButton(
+            child: Text('Create Record'),
+            onPressed: () {
+              createRecord();
+            },
+          ),
+          RaisedButton(
+            child: Text('View Record'),
+            onPressed: () {
+              getData();
+            },
+          ),
+        ],
+      ));
+}
+}
---
+++ lib/ListBuilder/listbuilder.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+List<String> items = [
+  // Football(firstName: 'Joe', position: 'QB', college: 'LSU' ),
+  // Football(firstName: 'Ed', position: 'Head Coach', college: 'LSU' ),
+  // Football(firstName: 'Steve', position: 'Coordinator', college: 'LSU' )
+  'football1',
+  'football2',
+  'football3'
+];
+
+class ListItems extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    print(items);
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              //onTap: () {},
+              title: Text(items[index]),
+            ),
+          );
+        });
+  }
+}
