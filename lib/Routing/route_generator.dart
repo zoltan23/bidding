661d8f597092213de498876d8e7c5f94a75b6620
Tim <tim@Tims-MacBook-Air.local>
2020-02-04 13:25:26 -0700

    fixed route_generator conflict

--- lib/Routing/route_generator.dart
+++ lib/Routing/route_generator.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/Landing/landing_page.dart';
-<<<<<<< HEAD
 import 'package:hello_world/Login/signup.dart';
-=======
 import 'package:hello_world/sevices/settings.dart';
->>>>>>> settings
 import '../Login/login.dart';
 import '../ListBuilder/firebase_crud.dart';
 import '../HomePage/home_page.dart';
 
 class RouteGenerator {
-  //String data = 'Test'; 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     //final args = settings.arguments;
 
@@ -23,13 +19,10 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => LandingPage());
       case '/firebase':
         return MaterialPageRoute(builder: (_) => FirestoreData());
-<<<<<<< HEAD
       case '/signup':
         return MaterialPageRoute(builder: (_) => SignUp());
-=======
       case '/settings':
         return MaterialPageRoute(builder: (_) => Settings());  
->>>>>>> settings
       // case '/second':
       //   if (args is String) {
       //     return MaterialPageRoute(
